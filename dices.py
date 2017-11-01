555cb3456701725dc735ea70807a120692cf696c
dekartkain <dkain@yandex.ru>
2017-11-02 01:49:26 +0300

    Create dices.py

---
+++ dices.py
@@ -0,0 +1,31 @@
+dice1 = "┌───────┐\n│                 │\n│       ●        │\n│                 │\n└───────┘"
+dice2 = "┌───────┐\n│   ●            │\n│                 │\n│            ●   │\n└───────┘"
+dice3 = "┌───────┐\n│   ●            │\n│       ●        │\n│            ●   │\n└───────┘"
+dice4 = "┌───────┐\n│   ●       ●   │\n|                 |\n│   ●       ●   │\n└───────┘"
+dice5 = "┌───────┐\n│   ●       ●   │\n│       ●        │\n│   ●       ●   │\n└───────┘"
+dice6 = "┌───────┐\n│   ●       ●   │\n│   ●       ●   │\n│   ●       ●   │\n└───────┘"
+
+dice_lib = {
+    1: dice1,
+    2: dice2,
+    3: dice3,
+    4: dice4,
+    5: dice5,
+    6: dice6
+}
+
+dice1Id = "CAADAgADxgAD3-bIBMHHnkQBHIZeAg"
+dice2Id = "CAADAgADxwAD3-bIBKh5M4W-LsGgAg"
+dice3Id = "CAADAgADyAAD3-bIBNNM1srKKmrCAg"
+dice4Id = "CAADAgADyQAD3-bIBHwniG3TyQV8Ag"
+dice5Id = "CAADAgADygAD3-bIBAHgF5geMgxEAg"
+dice6Id = "CAADAgADywAD3-bIBJ_9MsP4PbtuAg"
+
+dice_id_lib = {
+    1: dice1Id,
+    2: dice2Id,
+    3: dice3Id,
+    4: dice4Id,
+    5: dice5Id,
+    6: dice6Id
+}
