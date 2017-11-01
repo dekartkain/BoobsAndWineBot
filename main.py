65ffa2d7db1396177ada46023f1d3d71179bde93
dekartkain <dkain@yandex.ru>
2017-11-02 01:55:50 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask, request
+
+import dices
+import rm
+import telebot
+from random import randint
+
+TOKEN = os.environ['PP_BOT_TOKEN']
+URL = os.environ['PP_BOT_URL']
+REPO = os.environ['PP_BOT_REPO']
+SECRET = '/' + TOKEN
+
+
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Cheer, ' + message.from_user.first_name)
+    bot.send_message(message.chat.id, message.from_user.id)
