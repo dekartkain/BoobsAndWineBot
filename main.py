29dce46dbdc05cf5e395ff05fa0be07b9316bf2e
dekartkain <dkain@yandex.ru>
2017-11-02 02:36:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,15 @@ def start(message):
     bot.reply_to(message, 'Hi, ' + message.from_user.first_name)
     bot.send_message(message.chat.id, message.from_user.id)
     
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
+    
+    
+    
 @server.route(SECRET, methods=['POST'])
 def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
