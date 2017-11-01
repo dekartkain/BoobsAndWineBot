d7610f2238f08d7295f71d43dd6a0abb3a228091
dekartkain <dkain@yandex.ru>
2017-11-02 02:40:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,6 @@ def start(message):
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
 
-if __name__ == '__main__':
-     bot.polling(none_stop=True)
     
     
     
