ec241102cff2746e8bca65aa8240409603f0e187
dekartkain <dkain@yandex.ru>
2017-11-02 02:17:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hi, ' + message.from_user.first_name + '. You id:' + message.from_user.id)
+    bot.reply_to(message, 'Hi, ' + message.from_user.first_name)
     bot.send_message(message.chat.id, message.from_user.id)
     
 @server.route(SECRET, methods=['POST'])
