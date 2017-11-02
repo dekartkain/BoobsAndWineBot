dc159d1bc27f54c926d52d6ebbadaa18a7446182
dekartkain <dkain@yandex.ru>
2017-11-03 01:27:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,35 +27,20 @@ def synboobslst(message):
 	for i in range(len(syn_boobs.syn_boobs_lib)):
 		       bot.send_message(message.chat.id, syn_boobs.syn_boobs_lib[i])
 
-#вино
-@bot.message_handler(content_types=['text'])
-def winemsg(message):
-	if message.text == 'вино' or 'Вино' or 'винцо' or 'Винцо' or 'винишко' or 'Винишко':
-		bot.send_message(message.chat.id, 'Я больше по сиськам...')
-	else:
-		exmp = message.text
-		exmp_lower = exmp.lower() #сделать все символы строчными
-		synboobslist = syn_boobs.syn_boobs_lib #подтягиваем словарь синонимов
-		for i in range(len(synboobslist)):
-			if exmp_lower.find(synboobslist[i]) != -1:
-				rand_val = randint(1, 76) #кол-во фоток
-				boobs_img = 'http://boobsandwinebot.freedynamicdns.net/localhost/www/boobs/' + str(rand_val) + '.jpg'
-				bot.send_photo(message.chat.id, boobs_img)
-				break
 			
 #парсер	сисек
-#@bot.message_handler(content_types=['text'])
-#def parser(message):
-#	exmp = message.text
-#	exmp_lower = exmp.lower() #сделать все символы строчными
+@bot.message_handler(content_types=['text'])
+def parser(message):
+	exmp = message.text
+	exmp_lower = exmp.lower() #сделать все символы строчными
 #	exmp_lower_nosp = exmp_lower.replace(' ','') #удалить проблеиы (' ')
-#	synboobslist = syn_boobs.syn_boobs_lib #подтягиваем словарь синонимов
-#	for i in range(len(synboobslist)):
-#		if exmp_lower.find(synboobslist[i]) != -1:
-#			rand_val = randint(1, 76) #кол-во фоток
-#			boobs_img = 'http://boobsandwinebot.freedynamicdns.net/localhost/www/boobs/' + str(rand_val) + '.jpg'
-#			bot.send_photo(message.chat.id, boobs_img)
-#			break
+	synboobslist = syn_boobs.syn_boobs_lib #подтягиваем словарь синонимов
+	for i in range(len(synboobslist)):
+		if exmp_lower.find(synboobslist[i]) != -1:
+			rand_val = randint(1, 76) #кол-во фоток
+			boobs_img = 'http://boobsandwinebot.freedynamicdns.net/localhost/www/boobs/' + str(rand_val) + '.jpg'
+			bot.send_photo(message.chat.id, boobs_img)
+			break
 		
 
 		
