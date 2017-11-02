291d6152f4e7d50b6a8b44a3a214448f20733eba
dekartkain <dkain@yandex.ru>
2017-11-02 03:00:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ server = Flask(__name__)
 @bot.message_handler(commands=['start'])
 def start(message):
 #    bot.reply_to(message, 'Hi, ' + message.from_user.first_name)
-    bot.send_message(message.chat.id, 'Hi, ' + message.from_user.first_name, message.from_user.id)
-#    bot.send_message(message.chat.id, message.from_user.id)
+    bot.send_message(message.chat.id, 'Hi, ' + message.from_user.first_name)
+    bot.send_message(message.chat.id, message.from_user.id)
+    bot.send_message(message.chat.id, user.username)
 
 #повторяем сообщение
 @bot.message_handler(content_types=["text"])
