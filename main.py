9bbca565a80e011c568a38ab300af430a44ff40a
dekartkain <dkain@yandex.ru>
2017-11-02 04:08:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from random import randint
 TOKEN = os.environ['PP_BOT_TOKEN']
 URL = os.environ['PP_BOT_URL']
 REPO = os.environ['PP_BOT_REPO']
-#SECRET = '/' + TOKEN
+SECRET = '/' + TOKEN
 
 
 bot = telebot.TeleBot(TOKEN)
@@ -37,15 +37,15 @@ def start(message):
     
     
     
-#@server.route(SECRET, methods=['POST'])
-#def get_message():
-#    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-#    return "POST", 200
+@server.route(SECRET, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "POST", 200
        
-#@server.route("/")
-#def web_hook():
-#    bot.remove_webhook()
-#    bot.set_webhook(url=URL+SECRET)
-#    return "CONNECTED", 200
+@server.route("/")
+def web_hook():
+    bot.remove_webhook()
+    bot.set_webhook(url=URL+SECRET)
+    return "CONNECTED", 200
 
-#server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))  
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))  
