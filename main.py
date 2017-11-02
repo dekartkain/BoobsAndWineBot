7cf1ef386c5866b1ea32f464687c7475c543a435
dekartkain <dkain@yandex.ru>
2017-11-02 18:00:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request
 import telebot
 from random import randint
+import requests
 
 TOKEN = os.environ['PP_BOT_TOKEN']
 URL = os.environ['PP_BOT_URL']
@@ -18,10 +19,10 @@ def start(message):
 	bot.send_message(message.chat.id, 'Привет, ' + message.from_user.first_name + '... Твой id: ' + str(message.from_user.id))
 	bot.send_message(message.chat.id, 'скоро я смогу налить тебе винца и показать пару красивых сисек!')
 
-@bot.message_handler(content_types=['text'])
-def roll(message):
-	if message.text == 'сиськи!':
-		bot.send_photo(message.chat.id, photo='http://voshod.tk/promo/img/fin0.png')
+#@bot.message_handler(content_types=['text'])
+#def roll(message):
+#	if message.text == 'сиськи!':
+#		bot.send_photo(message.chat.id, photo='http://voshod.tk/promo/img/fin0.png')
     
 	
 
