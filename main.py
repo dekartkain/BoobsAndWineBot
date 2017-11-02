e4d4526739dac6998c66146a3828ebf3cf02f47f
dekartkain <dkain@yandex.ru>
2017-11-02 04:06:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,15 +19,14 @@ server = Flask(__name__)
 #приветствие, id
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
-#    bot.reply_to(message, 'Hi, ' + message.from_user.first_name)
     bot.send_message(message.chat.id, 'Привет, ' + message.from_user.first_name + '... Твой id: ' + str(message.from_user.id))
   
 
 
 #ответ по слову
-@bot.message_handler(filters)
-def answer_by_pass(message):
-	bot.reply_to(message, "This is a message handler")
+#@bot.message_handler(filters)
+#def answer_by_pass(message):
+#	bot.reply_to(message, "This is a message handler")
     
 
 #повторяем сообщение
