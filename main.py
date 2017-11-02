648544c5ac743d7b6305f3e2ec01d93aa90e3ac4
dekartkain <dkain@yandex.ru>
2017-11-02 14:27:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import os
+from random import randint
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 TOKEN = os.environ['PP_BOT_TOKEN']
 URL = os.environ['PP_BOT_URL']
 REPO = os.environ['PP_BOT_REPO']
+
+bot = telebot.TeleBot(TOKEN)
 
 ################################################################################################################
 
