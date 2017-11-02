4074df8faf60fba53f45aa7d4f54d93732fe3c8f
dekartkain <dkain@yandex.ru>
2017-11-02 21:46:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ def test(message):
 		
 
 #ответ по слову
-@bot.message_handler(content_types=['text'])
-def answer_by_pass(message):
-	if message.text == 'сиськи' or 'Сиськи' or '1':
-		rand_val = randint(1, 76)
-		boobs_img = 'http://boobsandwinebot.freedynamicdns.net/localhost/www/boobs/' + str(rand_val) + '.jpg'
-		bot.send_photo(message.chat.id, boobs_img)
+#@bot.message_handler(content_types=['text'])
+#def answer_by_pass(message):
+#	if message.text == 'сиськи' or 'Сиськи' or '1':
+#		rand_val = randint(1, 76)
+#		boobs_img = 'http://boobsandwinebot.freedynamicdns.net/localhost/www/boobs/' + str(rand_val) + '.jpg'
+#		bot.send_photo(message.chat.id, boobs_img)
 
 
 		
