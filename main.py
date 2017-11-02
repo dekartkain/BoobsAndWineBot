a8f714db4a20c587da816cf42e90bd95f13bf383
dekartkain <dkain@yandex.ru>
2017-11-02 15:14:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ TOKEN = os.environ['PP_BOT_TOKEN']
 URL = os.environ['PP_BOT_URL']
 REPO = os.environ['PP_BOT_REPO']
 
-bot = telegram.Bot(TOKEN)
+def start(bot, update):
+  bot.sendMessage(chat_id=update.message.chat_id, text="Здравствуйте.")
+  
+updater = Updater(token=TOKEN)
+start_handler = CommandHandler('start', start)
+updater.dispatcher.add_handler(start_handler)
+updater.start_polling()
 
-bot.send_messge(update.message.chat_id, '123')
+
+#bot = telegram.Bot(TOKEN)
+#bot.send_messge(update.message.chat_id, '123')
